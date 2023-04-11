b8e3594db2960ab4abad63000dec1083ba49efa3
xthzb <xthzb@qq.com>
2023-04-12 01:41:19 +0800

    1

---
+++ hview.py
@@ -0,0 +1,39 @@
+from tkinter import *
+
+class HForm(Tk):
+    def __init__(self, screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = True, sync: bool = False, use: str | None = None) -> None:
+        super().__init__(screenName, baseName, className, useTk, sync, use)
+        self.text='HForm'
+        self.sw = self.winfo_screenwidth()
+        self.sh = self.winfo_screenheight()
+        self.width=300
+        self.height=250
+        self.x=(self.sw-self.width) / 2
+        self.y=(self.sh-self.height) / 2-60
+        
+
+    def __setattr__(self, __name: str, __value: any) -> None:
+        returnVal=super().__setattr__(__name, __value)
+        match __name:
+            case 'text':
+                self.title(__value)
+            case 'width':
+                self.geometry('%dx%d' %(__value,self.winfo_height()))
+                self.update()
+                pass
+            case 'height':
+                self.geometry('%dx%d' %(self.winfo_width(),__value))
+                self.update()
+                pass
+            case 'x':
+                self.geometry("%dx%d+%d+%d" %(self.width,self.height,__value,self.winfo_y()))
+                self.update()
+            case 'y':
+                self.geometry("%dx%d+%d+%d" %(self.width,self.height,self.winfo_x(),__value))
+                self.update()
+        return returnVal
+        
+    
+    def show(self):
+        self.mainloop()
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-#332211
-print('12')
-print('112233')
-#112233
+from hview import *
+
+def g():
+    frm.x+=10
+    frm.y+=10
+
+frm=HForm()
+frm.text='窗口'
+Button(frm,text='确 定',bg='Ivory',font=('微软雅黑',12),width=0,height=0,command=g).place(x=5,y=5)
+frm.show()
